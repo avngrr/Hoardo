5a1f867fe0294fc4636dcf44059552bc5bac45db
avngrr <enzodesmidt@gmail.com>
2022-07-20 20:27:37 +0200

    Add fastendpoints

---
+++ src/Server/Endpoints/Movies/GetAllMoviesEndpoint.cs
@@ -0,0 +1,10 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Server.Endpoints.Movies;
+
+[HttpGet("movies"), AllowAnonymous]
+public class GetAllMoviesEndpoint
+{
+
+}
