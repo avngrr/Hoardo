c0ad607f1e6fda5c53623a6ba7a55031669d3320
avngrr <enzodesmidt@gmail.com>
2022-07-24 20:13:03 +0200

    IMovieService

---
+++ src/Application/Common/Interfaces/Movies/IMovieService.cs
@@ -0,0 +1,15 @@
+﻿using Application.Features.Movies.Queries;
+using Application.Features.Movies.Responses;
+using Domain.Entities.Movies;
+using Shared.Wrappers;
+
+namespace Application.Common.Interfaces.Movies;
+public interface IMovieService
+{
+    Task AddAsync(Movie movie);
+    Task<Movie> GetAsync(int id);
+    Task DeleteAsync(int id);
+    Task UpdateAsync(int id);
+    Task<List<Movie>> GetAllAsync();
+    Task<PagedList<Movie>> GetAllPaged(int pageNumber, int pageSize);
+}
--- src/Server/Endpoints/Movies/GetAllMoviesEndpoint.cs
+++ src/Server/Endpoints/Movies/GetAllMoviesEndpoint.cs
@@ -13,22 +13,22 @@ namespace Server.Endpoints.Movies;
 [HttpGet("movies"), AllowAnonymous]
 public class GetAllMoviesEndpoint : Endpoint<GetAllPagedMoviesQuery, GetAllPagedMovieResponse>
 {
-    //private readonly IUnitOfWork<int> _unitOfWork;
-    //private readonly IMapper _mapper;
-    //public GetAllMoviesEndpoint(IUnitOfWork<int> unitOfWork, IMapper mapper)
-    //{
-    //    _unitOfWork = unitOfWork;
-    //    _mapper = mapper;   
-    //}
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    public GetAllMoviesEndpoint(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;   
+    }
 
     public override async Task HandleAsync(GetAllPagedMoviesQuery query, CancellationToken cancellationToken)
     {
-        //var moviesList = await _unitOfWork.Repository<Movie>().GetPagedResponseAsync(query.PageNumber, query.PageSize);
-        //var mappedMoviesList = _mapper.Map<PagedList<MovieResponse>>(moviesList);
-        //GetAllPagedMovieResponse result = new GetAllPagedMovieResponse()
-        //{
-        //    MovieList = mappedMoviesList
-        //};
+        var moviesList = await _unitOfWork.Repository<Movie>().GetPagedResponseAsync(query.PageNumber, query.PageSize);
+        var mappedMoviesList = _mapper.Map<List<MovieResponse>>(moviesList);
+        GetAllPagedMovieResponse result = new GetAllPagedMovieResponse()
+        {
+            MovieList = new PagedList<MovieResponse>(mappedMoviesList, mappedMoviesList.Count, query.PageNumber, query.PageSize)
+        };
         await SendOkAsync(cancellationToken);
     }
 }
